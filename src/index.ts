5f284c92b1cc23caa910613401b5e73b8aee6d99
Varunkumar8055 <149817216+Varunkumar8055@users.noreply.github.com>
2025-03-11 15:57:35 +0530

    Add health check and reminders endpoints to the Hono server

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
+const reminders: string[] = [];
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get("/health", (context) => {
+  const req = context.req;
+  return context.json({"message": "Hello, World!"}, 200);
+});
+
+app.get("/reminders", (context) => {
+  return context.json(reminders, 200);
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.post("/reminders", async (context) => {
+  //const body = (await context.req.parseBody()) as Record<string, string>;
+  const body = await context.req.json();
+  console.log(body);
+  const reminder = body.reminder;
+  reminders.push(body.reminder);
+  return context.json(reminders, 201);
+});
+
+serve(app);
+
+console.log("server is runnning on port 3000");
